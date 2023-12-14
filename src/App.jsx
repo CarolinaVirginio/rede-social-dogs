8639235568d499ace54642165cbb0285e42ceee0
CarolinaVirginio <carolinavir35@gmail.com>
2023-12-14 18:55:00 -0300

    Animação e alguns links do login e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import './App.css';
+import  './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import {UserStorage} from './UserContext'
+import {UserStorage} from './UserContext';
 
 function App() {
   return (

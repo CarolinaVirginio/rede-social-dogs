27c8b470ec5e6ad156764e86ec9fca47d9aac354
CarolinaVirginio <carolinavir35@gmail.com>
2023-12-18 15:23:43 -0300

    Menu Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import  './App.css';
+import React from 'react';
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import {UserStorage} from './UserContext';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRouter from './Helper/ProtectedRouter';
 
 function App() {
   return (
@@ -14,14 +16,18 @@ function App() {
         <UserStorage>
           <Header />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login/*' element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={
+            <ProtectedRouter>
+              <User />
+            </ProtectedRouter>} />
           </Routes>
           <Footer />
         </UserStorage>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
